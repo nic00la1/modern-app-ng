0b0ec8dcb27e621a9673638b065d58326e5fa0bc
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-08 07:34:38 +0100

    Add movie loading functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,8 +4,10 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  InfiniteScrollCustomEvent,
 } from '@ionic/angular/standalone';
 import { MovieService } from '../services/movie.service';
+import { catchError, finalize } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -16,13 +18,41 @@ import { MovieService } from '../services/movie.service';
 })
 export class HomePage {
   private movieService = inject(MovieService);
+  private currentPage = 1;
+  private error = null;
+  private isLoading = false;
+  private movies = [];
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
+
   constructor() {
     this.loadMovies();
   }
 
-  loadMovies() {
-    this.movieService.getMovieDetails('1029575').subscribe((movies) => {
-      console.log(movies);
-    });
+  loadMovies(event?: InfiniteScrollCustomEvent) {
+    this.error = null;
+
+    if (!event) {
+      this.isLoading = true;
+    }
+
+    this.movieService
+      .getTopRatedMovies(this.currentPage)
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          if (event) {
+            event.target.complete();
+          }
+        }),
+        catchError((err: any) => {
+          console.log(err);
+
+          this.error = err.error.status_message;
+          return [];
+        })
+      )
+      .subscribe();
   }
+
+  loadMore(event: InfiniteScrollCustomEvent) {}
 }
